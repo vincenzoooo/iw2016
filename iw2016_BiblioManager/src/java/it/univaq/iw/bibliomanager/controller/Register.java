85e785f3b7bef55c79db01adbe64711d7e0c0781
Vincenzo <Vincenzo@Vincenzo-PC>
2016-07-02 13:34:22 +0200

    Modificata registrazione

--- iw2016_BiblioManager/src/java/it/univaq/iw/bibliomanager/controller/Register.java
+++ iw2016_BiblioManager/src/java/it/univaq/iw/bibliomanager/controller/Register.java
@@ -6,7 +6,6 @@
  */
 package it.univaq.iw.bibliomanager.controller;
 
-import it.univaq.iw.bibliomanager.data.model.Utente;
 import it.univaq.iw.framework.data.DataLayerException;
 import it.univaq.iw.framework.result.TemplateResult;
 import it.univaq.iw.framework.utils.Utils;
@@ -16,6 +15,7 @@ import javax.mail.MessagingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import it.univaq.iw.bibliomanager.data.model.User;
 
 /**
  *
@@ -26,29 +26,68 @@ public class Register extends BiblioManagerBaseController {
     private void action_default(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         request.setAttribute("page_title", "Register to Biblio");
         TemplateResult res = new TemplateResult(getServletContext());
-        res.activate("register.ftl.html", request, response);
+        res.activate("registration.ftl.html", request, response);
     }
 
     private void action_register(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException, DataLayerException, NoSuchAlgorithmException, MessagingException {
-        TemplateResult res = new TemplateResult(getServletContext());
-        String name = Utils.checkString(request.getParameter("name"));
-        String surname = Utils.checkString(request.getParameter("surname"));
-        String password = Utils.checkString(request.getParameter("password"));
-        String email = Utils.checkString(request.getParameter("email"));
-        if (!Utils.checkEmail(email)) {
-            //TODO: invio degli errori indietro alla pagina
-            throw new IOException("Input error: [" + email + "] is not a correct email"); //TODO: Brutale da cambiare
-        } else {
-            Utente newUser = getDataLayer().createUser();
-            newUser.setName(name);
-            newUser.setSurname(surname);
-            newUser.setPassword(Utils.encryptPassword(password));
-            newUser.setEmail(email);
-            getDataLayer().storeUser(newUser);
-            String text = "Benvenuto su BiblioManager!";
-            Utils.sendEmail(email, text);
-            res.activate("index.html", request, response);
+        try {
+            TemplateResult res = new TemplateResult(getServletContext());
+            String name = Utils.checkString(request.getParameter("name"));
+            String surname = Utils.checkString(request.getParameter("surname"));
+            String password = Utils.checkString(request.getParameter("password"));
+            String rePassword = Utils.checkString(request.getParameter("re-password"));
+            String email = Utils.checkString(request.getParameter("email"));
+            String reEmail = Utils.checkString(request.getParameter("re-email"));
+            String privacy = Utils.checkString(request.getParameter("privacy"));
+            boolean error = false;
+            if (name == null) {
+                request.setAttribute("errorName", "Nome non valorizzato");
+                error = true;
+            }
+            if (surname == null) {
+                request.setAttribute("errorSurname", "Cognome non valorizzato");
+                error = true;
+            }
+            if (password == null || rePassword == null) {
+                request.setAttribute("errorPassword", "Password o Repassword non valorizzato");
+                error = true;
+            } else if (!password.equals(rePassword)) {
+                request.setAttribute("errorPassword", "I campi Password e Repassword non sono uguali");
+                error = true;
+            }
+            if (email == null || reEmail == null) {
+                request.setAttribute("errorEmail", "Email non valorizzata");
+                error = true;
+            } else if (!email.equals(reEmail)) {
+                request.setAttribute("errorEmail", "I campi email e Reemail non sono uguali");
+                error = true;
+            }
+            if(privacy == null){
+                request.setAttribute("errorPrivacy", "Bisogna accettare i termini di legge sulla privacy");
+                error = true;
+            }
+            if (!Utils.checkEmail(email)) {
+                request.setAttribute("errorEmail", "L'Email passata non è valida");
+                error = true;
+            } 
+            if(!error){
+                User newUser = getDataLayer().createUser();
+                newUser.setName(name);
+                newUser.setSurname(surname);
+                newUser.setPassword(Utils.encryptPassword(password));
+                newUser.setEmail(email);
+                getDataLayer().storeUser(newUser);
+                //TODO: Controllare l'invio email
+                //String text = "Benvenuto su BiblioManager!";
+                //Utils.sendEmail(email, text);
+                res.activate("index.html", request, response);
+            }
+            else{
+                res.activate("registration.ftl.html", request, response);
+            }
+        } catch (DataLayerException ex) {
+            action_error(request, response, "Error: " + ex.getMessage());
         }
     }
 
@@ -64,7 +103,7 @@ public class Register extends BiblioManagerBaseController {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException {
         try {
-            if (request.getParameterNames() != null && request.getParameter("name") != null && request.getParameter("surname") != null && request.getParameter("password") != null && request.getParameter("email") != null) {
+            if (request.getParameterNames() != null) {
                 action_register(request, response);
             } else {
                 action_default(request, response);
