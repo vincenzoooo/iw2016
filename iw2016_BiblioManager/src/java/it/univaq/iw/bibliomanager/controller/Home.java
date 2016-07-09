1b90096baeca1d4f7acf26543ef06502bfa1f29d
Angelo Iezzi <i_gelo@hotmail.it>
2016-07-09 23:53:30 +0200

    Modifiche grafiche, home statica completata!

--- iw2016_BiblioManager/src/java/it/univaq/iw/bibliomanager/controller/Home.java
+++ iw2016_BiblioManager/src/java/it/univaq/iw/bibliomanager/controller/Home.java
@@ -6,6 +6,7 @@
  */
 package it.univaq.iw.bibliomanager.controller;
 
+import it.univaq.iw.bibliomanager.data.model.BiblioManagerDataLayer;
 import it.univaq.iw.framework.data.DataLayerException;
 import it.univaq.iw.framework.result.TemplateResult;
 import it.univaq.iw.framework.security.SecurityLayer;
@@ -16,12 +17,42 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import it.univaq.iw.bibliomanager.data.model.User;
+import it.univaq.iw.framework.result.TemplateManagerException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author Angelo Iezzi
  */
 public class Home extends BiblioManagerBaseController {
+    
+     private void action_default(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.setAttribute("page_title", "Home");
+        TemplateResult res = new TemplateResult(getServletContext());
+        res.activate("home.ftl.html", request, response);
+    }
+     
+    private void action_logged(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, DataLayerException {
+        
+        HttpSession s = SecurityLayer.checkSession(request);
+        if(s!=null){
+            request.setAttribute("page_title", "Homepage");
+            // recupero lo userId e il nome
+            int userId = (int) s.getAttribute("userid");
+            boolean role = (boolean) s.getAttribute("userRole");
+            System.out.println("userId: "+userId);
+            User us = ((BiblioManagerDataLayer) request.getAttribute("datalayer")).getUser(userId);
+            String nameLogged = SecurityLayer.stripSlashes(us.getName());
+            request.setAttribute("nameLogged", nameLogged);
+            request.setAttribute("usernameLogged", "Homepage");
+            TemplateResult res = new TemplateResult(getServletContext());
+            request.setAttribute("role", role);
+            
+            res.activate("home.ftl.html", request, response);
+        }
+    }
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -34,6 +65,11 @@ public class Home extends BiblioManagerBaseController {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException {
+         try {
+             action_default(request, response);
+         } catch (IOException ex) {
+             Logger.getLogger(Home.class.getName()).log(Level.SEVERE, null, ex);
+         }
        
     }
 
