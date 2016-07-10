e25ae73e3b593478e2f85d6f308159105d5093e3
Vincenzo Lanzieri <lanzieri.vincenzo@gmail.com>
2016-07-10 19:47:01 +0200

    Update Home.java

--- iw2016_BiblioManager/src/java/it/univaq/iw/bibliomanager/controller/Home.java
+++ iw2016_BiblioManager/src/java/it/univaq/iw/bibliomanager/controller/Home.java
@@ -6,20 +6,16 @@
  */
 package it.univaq.iw.bibliomanager.controller;
 
-import it.univaq.iw.bibliomanager.data.model.BiblioManagerDataLayer;
+import it.univaq.iw.bibliomanager.data.model.Publication;
 import it.univaq.iw.framework.data.DataLayerException;
 import it.univaq.iw.framework.result.TemplateResult;
 import it.univaq.iw.framework.security.SecurityLayer;
-import it.univaq.iw.framework.utils.Utils;
 import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import it.univaq.iw.bibliomanager.data.model.User;
-import it.univaq.iw.framework.result.TemplateManagerException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.List;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -36,24 +32,22 @@ public class Home extends BiblioManagerBaseController {
      
     private void action_logged(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, DataLayerException {
         
-        HttpSession s = SecurityLayer.checkSession(request);
-        if(s!=null){
+        HttpSession session = SecurityLayer.checkSession(request);
+        if(session!=null){
             request.setAttribute("page_title", "Homepage");
             // recupero lo userId e il nome
-            int userId = (int) s.getAttribute("userid");
-            boolean role = (boolean) s.getAttribute("userRole");
-            System.out.println("userId: "+userId);
-            User us = ((BiblioManagerDataLayer) request.getAttribute("datalayer")).getUser(userId);
-            String nameLogged = SecurityLayer.stripSlashes(us.getName());
+            int userId = (int) session.getAttribute("userid");
+            boolean role = (boolean) session.getAttribute("userRole");
+            User user = getDataLayer().getUser(userId);
+            String nameLogged = SecurityLayer.stripSlashes(user.getName());
             request.setAttribute("nameLogged", nameLogged);
             request.setAttribute("usernameLogged", "Homepage");
             TemplateResult res = new TemplateResult(getServletContext());
             request.setAttribute("role", role);
-            
             res.activate("home.ftl.html", request, response);
         }
     }
-
+    
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -61,16 +55,25 @@ public class Home extends BiblioManagerBaseController {
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
      */
+    @Override
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException {
          try {
-             action_default(request, response);
-         } catch (IOException ex) {
-             Logger.getLogger(Home.class.getName()).log(Level.SEVERE, null, ex);
-         }
-       
+            List<Publication> publicationsInsert = getDataLayer().getLastInsertedPublication(); //Select BY InsertDate
+            List<Publication> publicationsUpdate = getDataLayer().getLastModifiedPublication(); //Select BY LastUpdate
+            List<User> activeUsers = getDataLayer().getMoreActiveUsers(); //Select BY Max publication insert
+            request.setAttribute("lastPublicationsInsert", publicationsInsert);
+            request.setAttribute("lastPublicationsUpdate", publicationsUpdate);
+            request.setAttribute("activeUsers", activeUsers);
+            if (SecurityLayer.checkSession(request) != null) {
+                action_logged(request, response);
+            } else {
+                action_default(request, response);
+            }
+        } catch (Exception ex) {
+            action_error(request, response, "OPS");
+        }
     }
 
     /**
