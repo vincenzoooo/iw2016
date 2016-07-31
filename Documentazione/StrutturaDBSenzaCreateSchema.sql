9dcaabf3d184bf19612ebd9456c05f15ea05bfa3
Vincenzo <Vincenzo@Vincenzo-PC>
2016-07-31 10:25:24 +0200

    Schema E/R e Struttura DB modificati

--- Documentazione/StrutturaDBSenzaCreateSchema.sql
+++ Documentazione/StrutturaDBSenzaCreateSchema.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 5.7.9, for Win32 (AMD64)
+-- MySQL dump 10.13  Distrib 5.7.10, for Win32 (AMD64)
 --
 -- Host: localhost    Database: iw2016
 -- ------------------------------------------------------
@@ -41,10 +41,10 @@ CREATE TABLE `autore_has_pubblicazione` (
   `autore_idautore` int(11) NOT NULL,
   `pubblicazione_idpubblicazione` int(11) NOT NULL,
   PRIMARY KEY (`autore_idautore`,`pubblicazione_idpubblicazione`),
-  KEY `fk_autore_has_pubblicazione_pubblicazione1_idx` (`pubblicazione_idpubblicazione`),
-  KEY `fk_autore_has_pubblicazione_autore_idx` (`autore_idautore`),
+  KEY `idx_autore_has_pubblicazione_pubblicazione` (`pubblicazione_idpubblicazione`),
+  KEY `idx_autore_has_pubblicazione_autore` (`autore_idautore`),
   CONSTRAINT `fk_autore_has_pubblicazione_autore` FOREIGN KEY (`autore_idautore`) REFERENCES `autore` (`idautore`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_autore_has_pubblicazione_pubblicazione1` FOREIGN KEY (`pubblicazione_idpubblicazione`) REFERENCES `pubblicazione` (`idpubblicazione`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_autore_has_pubblicazione_pubblicazione` FOREIGN KEY (`pubblicazione_idpubblicazione`) REFERENCES `pubblicazione` (`idpubblicazione`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -63,23 +63,16 @@ CREATE TABLE `editore` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `metadato`
+-- Table structure for table `keyword`
 --
 
-DROP TABLE IF EXISTS `metadato`;
+DROP TABLE IF EXISTS `keyword`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `metadato` (
-  `idmetadato` int(11) NOT NULL AUTO_INCREMENT,
-  `isbn` int(11) DEFAULT NULL,
-  `n_pagine` int(11) DEFAULT NULL,
-  `lingua` varchar(45) DEFAULT NULL,
-  `data_pubblicazione` date DEFAULT NULL,
-  `chiavi` varchar(45) DEFAULT NULL,
-  `pubblicazione` int(11) NOT NULL,
-  PRIMARY KEY (`idmetadato`),
-  KEY `fk_metadato_pubblicazione1_idx` (`pubblicazione`),
-  CONSTRAINT `fk_metadato_pubblicazione1` FOREIGN KEY (`pubblicazione`) REFERENCES `pubblicazione` (`idpubblicazione`) ON DELETE NO ACTION ON UPDATE NO ACTION
+CREATE TABLE `keyword` (
+  `idkeyword` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idkeyword`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -97,9 +90,30 @@ CREATE TABLE `pubblicazione` (
   `editore` int(11) NOT NULL,
   `indice` varchar(45) DEFAULT NULL,
   `n_consigli` int(11) DEFAULT NULL,
+  `isbn` int(11) DEFAULT NULL,
+  `n_pagine` int(11) DEFAULT NULL,
+  `lingua` varchar(45) DEFAULT NULL,
+  `data_pubblicazione` date DEFAULT NULL,
   PRIMARY KEY (`idpubblicazione`),
-  KEY `fk_pubblicazione_editore1_idx` (`editore`),
-  CONSTRAINT `fk_pubblicazione_editore1` FOREIGN KEY (`editore`) REFERENCES `editore` (`ideditore`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `idx_pubblicazione_editore` (`editore`),
+  CONSTRAINT `fk_pubblicazione_editore` FOREIGN KEY (`editore`) REFERENCES `editore` (`ideditore`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `pubblicazione_has_keyword`
+--
+
+DROP TABLE IF EXISTS `pubblicazione_has_keyword`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pubblicazione_has_keyword` (
+  `pubblicazione_idpubblicazione` int(11) NOT NULL,
+  `keyword_idkeyword` int(11) NOT NULL,
+  PRIMARY KEY (`pubblicazione_idpubblicazione`,`keyword_idkeyword`),
+  KEY `fk_pubblicazione_has_keyword_keyword_idx` (`keyword_idkeyword`),
+  CONSTRAINT `fk_pubblicazione_has_keyword_keyword` FOREIGN KEY (`keyword_idkeyword`) REFERENCES `keyword` (`idkeyword`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_pubblicazione_has_keyword_pubblicazione` FOREIGN KEY (`pubblicazione_idpubblicazione`) REFERENCES `pubblicazione` (`idpubblicazione`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -114,10 +128,10 @@ CREATE TABLE `pubblicazione_has_sorgente` (
   `pubblicazione_idpubblicazione` int(11) NOT NULL,
   `sorgente_idsorgente` int(11) NOT NULL,
   PRIMARY KEY (`pubblicazione_idpubblicazione`,`sorgente_idsorgente`),
-  KEY `fk_pubblicazione_has_sorgente_sorgente1_idx` (`sorgente_idsorgente`),
-  KEY `fk_pubblicazione_has_sorgente_pubblicazione1_idx` (`pubblicazione_idpubblicazione`),
-  CONSTRAINT `fk_pubblicazione_has_sorgente_pubblicazione1` FOREIGN KEY (`pubblicazione_idpubblicazione`) REFERENCES `pubblicazione` (`idpubblicazione`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_pubblicazione_has_sorgente_sorgente1` FOREIGN KEY (`sorgente_idsorgente`) REFERENCES `sorgente` (`idsorgente`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `idx_pubblicazione_has_sorgente_sorgente` (`sorgente_idsorgente`),
+  KEY `idx_pubblicazione_has_sorgente_pubblicazione` (`pubblicazione_idpubblicazione`),
+  CONSTRAINT `fk_pubblicazione_has_sorgente_pubblicazione` FOREIGN KEY (`pubblicazione_idpubblicazione`) REFERENCES `pubblicazione` (`idpubblicazione`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_pubblicazione_has_sorgente_sorgente` FOREIGN KEY (`sorgente_idsorgente`) REFERENCES `sorgente` (`idsorgente`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -136,12 +150,12 @@ CREATE TABLE `recensione` (
   `pubblicazione` int(11) NOT NULL,
   `storico` int(11) DEFAULT NULL,
   PRIMARY KEY (`idrecensione`),
-  KEY `fk_recensione_pubblicazione1_idx` (`pubblicazione`),
-  KEY `fk_recensione_utente1_idx` (`utente_autore`),
-  KEY `fk_recensione_storico1_idx` (`storico`),
-  CONSTRAINT `fk_recensione_pubblicazione1` FOREIGN KEY (`pubblicazione`) REFERENCES `pubblicazione` (`idpubblicazione`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_recensione_storico1` FOREIGN KEY (`storico`) REFERENCES `storico` (`idstorico`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_recensione_utente1` FOREIGN KEY (`utente_autore`) REFERENCES `utente` (`idutente`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `idx_recensione_pubblicazione` (`pubblicazione`),
+  KEY `idx_recensione_utente` (`utente_autore`),
+  KEY `idx_recensione_storico` (`storico`),
+  CONSTRAINT `fk_recensione_pubblicazione` FOREIGN KEY (`pubblicazione`) REFERENCES `pubblicazione` (`idpubblicazione`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_recensione_storico` FOREIGN KEY (`storico`) REFERENCES `storico` (`idstorico`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_recensione_utente` FOREIGN KEY (`utente_autore`) REFERENCES `utente` (`idutente`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -158,8 +172,8 @@ CREATE TABLE `ristampa` (
   `data` date DEFAULT NULL,
   `pubblicazione` int(11) NOT NULL,
   PRIMARY KEY (`idristampa`),
-  KEY `fk_ristampe_pubblicazione1_idx` (`pubblicazione`),
-  CONSTRAINT `fk_ristampe_pubblicazione1` FOREIGN KEY (`pubblicazione`) REFERENCES `pubblicazione` (`idpubblicazione`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `idx_ristampe_pubblicazione` (`pubblicazione`),
+  CONSTRAINT `fk_ristampe_pubblicazione` FOREIGN KEY (`pubblicazione`) REFERENCES `pubblicazione` (`idpubblicazione`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -195,10 +209,10 @@ CREATE TABLE `storico` (
   `pubblicazione` int(11) NOT NULL,
   `utente` int(11) NOT NULL,
   PRIMARY KEY (`idstorico`),
-  KEY `fk_storico_pubblicazione1_idx` (`pubblicazione`),
-  KEY `fk_storico_utente1_idx` (`utente`),
-  CONSTRAINT `fk_storico_pubblicazione1` FOREIGN KEY (`pubblicazione`) REFERENCES `pubblicazione` (`idpubblicazione`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_storico_utente1` FOREIGN KEY (`utente`) REFERENCES `utente` (`idutente`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `idx_storico_pubblicazione` (`pubblicazione`),
+  KEY `idx_storico_utente` (`utente`),
+  CONSTRAINT `fk_storico_pubblicazione` FOREIGN KEY (`pubblicazione`) REFERENCES `pubblicazione` (`idpubblicazione`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_storico_utente` FOREIGN KEY (`utente`) REFERENCES `utente` (`idutente`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -216,10 +230,7 @@ CREATE TABLE `utente` (
   `password` varchar(255) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
   `stato` tinyint(2) DEFAULT NULL,
-  `utente_attivo_idutente_passivo` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idutente`),
-  KEY `fk_attivo_passivo_idx` (`utente_attivo_idutente_passivo`),
-  CONSTRAINT `fk_attivo_passivo` FOREIGN KEY (`utente_attivo_idutente_passivo`) REFERENCES `utente` (`idutente`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  PRIMARY KEY (`idutente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -232,4 +243,4 @@ CREATE TABLE `utente` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-07-11 11:17:12
+-- Dump completed on 2016-07-31 10:19:13
