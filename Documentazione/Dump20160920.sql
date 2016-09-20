7cc47ac5bef57d7f418758cc06e147e0349f147f
Vincenzo Lanzieri <lanzieri.vincenzo@gmail.com>
2016-09-20 20:44:22 +0200

    Struttura DB

---
+++ Documentazione/Dump20160920.sql
@@ -0,0 +1,310 @@
+-- MySQL dump 10.13  Distrib 5.7.10, for Win32 (AMD64)
+--
+-- Host: localhost    Database: iw2016
+-- ------------------------------------------------------
+-- Server version	5.7.10-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `autore`
+--
+
+DROP TABLE IF EXISTS `autore`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `autore` (
+  `idautore` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) DEFAULT NULL,
+  `cognome` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idautore`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `autore_has_pubblicazione`
+--
+
+DROP TABLE IF EXISTS `autore_has_pubblicazione`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `autore_has_pubblicazione` (
+  `autore_idautore` int(11) NOT NULL,
+  `pubblicazione_idpubblicazione` int(11) NOT NULL,
+  PRIMARY KEY (`autore_idautore`,`pubblicazione_idpubblicazione`),
+  KEY `idx_autore_has_pubblicazione_autore` (`autore_idautore`),
+  KEY `fk_autore_has_pubblicazione_pubblicazione_idx` (`pubblicazione_idpubblicazione`),
+  CONSTRAINT `fk_autore_has_pubblicazione_autore` FOREIGN KEY (`autore_idautore`) REFERENCES `autore` (`idautore`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_autore_has_pubblicazione_pubblicazione` FOREIGN KEY (`pubblicazione_idpubblicazione`) REFERENCES `pubblicazione` (`idpubblicazione`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `capitolo`
+--
+
+DROP TABLE IF EXISTS `capitolo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `capitolo` (
+  `idcapitolo` int(11) NOT NULL AUTO_INCREMENT,
+  `numero` int(11) DEFAULT NULL,
+  `titolo` varchar(100) DEFAULT NULL,
+  `pubblicazione` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idcapitolo`),
+  KEY `fk_capitolo_pubblicazione_idx` (`pubblicazione`),
+  CONSTRAINT `fk_capitolo_pubblicazione` FOREIGN KEY (`pubblicazione`) REFERENCES `pubblicazione` (`idpubblicazione`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `consigli_utente`
+--
+
+DROP TABLE IF EXISTS `consigli_utente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `consigli_utente` (
+  `pubblicazione_idpubblicazione` int(11) NOT NULL,
+  `utente_idutente` int(11) NOT NULL,
+  PRIMARY KEY (`pubblicazione_idpubblicazione`,`utente_idutente`),
+  KEY `fk_consigli_utente_utente_idx` (`utente_idutente`),
+  CONSTRAINT `fk_consigli_utente_pubblicazione` FOREIGN KEY (`pubblicazione_idpubblicazione`) REFERENCES `pubblicazione` (`idpubblicazione`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fk_consigli_utente_utente` FOREIGN KEY (`utente_idutente`) REFERENCES `utente` (`idutente`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `editore`
+--
+
+DROP TABLE IF EXISTS `editore`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `editore` (
+  `ideditore` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`ideditore`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `filtri`
+--
+
+DROP TABLE IF EXISTS `filtri`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `filtri` (
+  `idfiltri` int(11) NOT NULL AUTO_INCREMENT,
+  `isbn` varchar(13) DEFAULT NULL,
+  `titolo` varchar(255) DEFAULT NULL,
+  `autore` varchar(255) DEFAULT NULL,
+  `editore` varchar(255) DEFAULT NULL,
+  `anno_inizio` varchar(45) DEFAULT NULL,
+  `anno_fine` varchar(45) DEFAULT NULL,
+  `keyword` varchar(45) DEFAULT NULL,
+  `lingua` varchar(45) DEFAULT NULL,
+  `download` varchar(45) DEFAULT NULL,
+  `utente` varchar(45) DEFAULT NULL,
+  `timestamp` datetime DEFAULT NULL,
+  PRIMARY KEY (`idfiltri`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `keyword`
+--
+
+DROP TABLE IF EXISTS `keyword`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `keyword` (
+  `idkeyword` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idkeyword`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `pubblicazione`
+--
+
+DROP TABLE IF EXISTS `pubblicazione`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pubblicazione` (
+  `idpubblicazione` int(11) NOT NULL AUTO_INCREMENT,
+  `titolo` varchar(255) DEFAULT NULL,
+  `descrizione` varchar(255) DEFAULT NULL,
+  `editore` int(11) DEFAULT NULL,
+  `n_consigli` int(11) DEFAULT NULL,
+  `isbn` varchar(13) DEFAULT NULL,
+  `n_pagine` int(11) DEFAULT NULL,
+  `lingua` varchar(45) DEFAULT NULL,
+  `data_pubblicazione` date DEFAULT NULL,
+  `incompleta` tinyint(1) DEFAULT NULL,
+  `timestamp` datetime DEFAULT NULL,
+  PRIMARY KEY (`idpubblicazione`),
+  KEY `idx_pubblicazione_editore` (`editore`),
+  CONSTRAINT `fk_pubblicazione_editore` FOREIGN KEY (`editore`) REFERENCES `editore` (`ideditore`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=129 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `pubblicazione_has_keyword`
+--
+
+DROP TABLE IF EXISTS `pubblicazione_has_keyword`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pubblicazione_has_keyword` (
+  `pubblicazione_idpubblicazione` int(11) NOT NULL,
+  `keyword_idkeyword` int(11) NOT NULL,
+  PRIMARY KEY (`pubblicazione_idpubblicazione`,`keyword_idkeyword`),
+  KEY `fk_pubblicazione_has_keyword_keyword_idx` (`keyword_idkeyword`),
+  CONSTRAINT `fk_pubblicazione_has_keyword_keyword` FOREIGN KEY (`keyword_idkeyword`) REFERENCES `keyword` (`idkeyword`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_pubblicazione_has_keyword_pubblicazione` FOREIGN KEY (`pubblicazione_idpubblicazione`) REFERENCES `pubblicazione` (`idpubblicazione`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `recensione`
+--
+
+DROP TABLE IF EXISTS `recensione`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `recensione` (
+  `idrecensione` int(11) NOT NULL AUTO_INCREMENT,
+  `testo` varchar(45) DEFAULT NULL,
+  `moderata` tinyint(1) DEFAULT NULL,
+  `data_recensione` datetime DEFAULT NULL,
+  `utente_autore` int(11) NOT NULL,
+  `pubblicazione` int(11) NOT NULL,
+  `storico` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idrecensione`),
+  KEY `idx_recensione_pubblicazione` (`pubblicazione`),
+  KEY `idx_recensione_utente` (`utente_autore`),
+  KEY `idx_recensione_storico` (`storico`),
+  CONSTRAINT `fk_recensione_pubblicazione` FOREIGN KEY (`pubblicazione`) REFERENCES `pubblicazione` (`idpubblicazione`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fk_recensione_storico` FOREIGN KEY (`storico`) REFERENCES `storico` (`idstorico`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_recensione_utente` FOREIGN KEY (`utente_autore`) REFERENCES `utente` (`idutente`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ristampa`
+--
+
+DROP TABLE IF EXISTS `ristampa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ristampa` (
+  `idristampa` int(11) NOT NULL AUTO_INCREMENT,
+  `numero` int(11) DEFAULT NULL,
+  `data` date DEFAULT NULL,
+  `pubblicazione` int(11) NOT NULL,
+  PRIMARY KEY (`idristampa`),
+  KEY `idx_ristampe_pubblicazione` (`pubblicazione`),
+  CONSTRAINT `fk_ristampe_pubblicazione` FOREIGN KEY (`pubblicazione`) REFERENCES `pubblicazione` (`idpubblicazione`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `sezione`
+--
+
+DROP TABLE IF EXISTS `sezione`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sezione` (
+  `idsezione` int(11) NOT NULL AUTO_INCREMENT,
+  `numero` int(11) DEFAULT NULL,
+  `titolo` varchar(100) DEFAULT NULL,
+  `capitolo` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idsezione`),
+  KEY `fk_sezione_capitolo_idx` (`capitolo`),
+  CONSTRAINT `fk_sezione_capitolo` FOREIGN KEY (`capitolo`) REFERENCES `capitolo` (`idcapitolo`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `sorgente`
+--
+
+DROP TABLE IF EXISTS `sorgente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sorgente` (
+  `idsorgente` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo` varchar(45) DEFAULT NULL,
+  `URI` varchar(255) DEFAULT NULL,
+  `formato` varchar(45) DEFAULT NULL,
+  `descrizione` varchar(255) DEFAULT NULL,
+  `pubblicazione` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idsorgente`),
+  KEY `fk_sorgente_pubblicazione_idx` (`pubblicazione`),
+  CONSTRAINT `fk_sorgente_pubblicazione` FOREIGN KEY (`pubblicazione`) REFERENCES `pubblicazione` (`idpubblicazione`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `storico`
+--
+
+DROP TABLE IF EXISTS `storico`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `storico` (
+  `idstorico` int(11) NOT NULL AUTO_INCREMENT,
+  `entry` varchar(45) DEFAULT NULL,
+  `tipo` int(11) DEFAULT NULL,
+  `data_operazione` datetime DEFAULT NULL,
+  `pubblicazione` int(11) NOT NULL,
+  `utente` int(11) NOT NULL,
+  PRIMARY KEY (`idstorico`),
+  KEY `idx_storico_pubblicazione` (`pubblicazione`),
+  KEY `idx_storico_utente` (`utente`),
+  CONSTRAINT `fk_storico_pubblicazione` FOREIGN KEY (`pubblicazione`) REFERENCES `pubblicazione` (`idpubblicazione`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fk_storico_utente` FOREIGN KEY (`utente`) REFERENCES `utente` (`idutente`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `utente`
+--
+
+DROP TABLE IF EXISTS `utente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `utente` (
+  `idutente` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) DEFAULT NULL,
+  `cognome` varchar(45) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `stato` tinyint(2) DEFAULT NULL,
+  PRIMARY KEY (`idutente`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-09-20 20:42:32
